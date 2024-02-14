d94b20aca63b7e3fc67217e134e08170d91bec74
baldvertebrate <rsaniel@sanagustinnhs.net>
2024-02-14 09:34:16 +0800

    Updated

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,7 @@ Route::delete('/admin/position/{positions}', [App\Http\Controllers\PositionContr
 Route::get('/admin/position/{positions}', [App\Http\Controllers\PositionController::class, 'modify'])->name('admin.position.modify');
 Route::put('/admin/position/{positions}', [App\Http\Controllers\PositionController::class, 'update'])->name('admin.position.update');
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
